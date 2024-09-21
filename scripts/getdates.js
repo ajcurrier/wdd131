2bf5e5b3d73bc3a1714b331dca502b0114973866
ajcurrier <aphspoo@byui.edu>
2024-09-21 17:39:21 -0600

    done? lets audit it

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,11 @@
 const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
 let lastModified = new Date(document.lastModified);
+
+document.getElementById('currentYear').textContent = currentYear;
+
+// code using template literals
+// document.getElementById('lastModified').textContent = `Last Modified: ${lastModified.toLocaleDateString()} ${lastModified.toLocaleTimeString()}`;
+
+// without using template literals
+document.getElementById('lastModified').textContent = 'Last Modified: ' + lastModified.toLocaleDateString() + ' ' + lastModified.toLocaleTimeString();
