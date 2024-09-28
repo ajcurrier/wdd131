345763f957f4edd95f5fe8ae461f1c6b95e1175d
ajcurrier <aphspoo@byui.edu>
2024-09-27 21:48:47 -0600

    awesome hamburger

--- scripts/temples.js
+++ scripts/temples.js
@@ -14,9 +14,13 @@ document.getElementById('lastModified').textContent = 'Last Modified: ' + lastMo
 // select the hamburger and menu elements
 const hamburger = document.getElementById('hamburger')
 const menu = document.getElementById('menu')
+const fadeElement = document.getElementById('fading-line')
 
 // Toggle the 'active' class on click
 hamburger.addEventListener('click', function() {
     console.log('hamburger menu clicked')
     menu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+    fadeElement.classList.toggle('active');
 })
+
