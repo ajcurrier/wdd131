3582eb315326214c648db4c7edb9b1cc59121e25
ajcurrier <aphspoo@byui.edu>
2024-10-09 14:27:25 -0600

    updage place

--- scripts/place.js
+++ scripts/place.js
@@ -4,6 +4,18 @@ let wind = 8;
 let windChill = 0;
 
 
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+let lastModified = new Date(document.lastModified);
+
+document.getElementById('currentYear').textContent = currentYear;
+
+// code using template literals
+// document.getElementById('lastModified').textContent = `Last Modified: ${lastModified.toLocaleDateString()} ${lastModified.toLocaleTimeString()}`;
+
+// without using template literals
+document.getElementById('lastModified').textContent = 'Last Modified: ' + lastModified.toLocaleDateString() + ' ' + lastModified.toLocaleTimeString();
+
 
 function calculateWindChill(temperature, wind) {
     if (temperature > 10 || wind <= 4.8) {
