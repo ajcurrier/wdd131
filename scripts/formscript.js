314bc8b8219136eb7c0cc8f9a25e6c3aea76fca1
ajcurrier <aphspoo@byui.edu>
2024-10-22 18:13:02 -0600

    review form

---
+++ scripts/formscript.js
@@ -0,0 +1,60 @@
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+
+  const select = document.getElementById('productName');
+
+  products.forEach(product => {
+    const productName = document.createElement('option')
+    // productName.value = product.name;
+    productName.textContent = product.name;
+    select.appendChild(productName);
+    
+  });
+
+
+
+
+
+
+
+
+
+
+
+// formscript
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+let lastModified = new Date(document.lastModified);
+
+document.getElementById('currentYear').textContent.currentYear;
+
+// code using template literals
+// document.getElementById('lastModified').textContent = `Last Modified: ${lastModified.toLocaleDateString()} ${lastModified.toLocaleTimeString()}`;
+
+// without using template literals
+document.getElementById('lastModified').textContent = 'Last Modified: ' + lastModified.toLocaleDateString() + ' ' + lastModified.toLocaleTimeString();
