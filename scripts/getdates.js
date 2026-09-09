980841fe151c28caaf18c85159bbf424bb9b27cc
ajcurrier <acurrier@byupathway.edu>
2026-09-09 17:16:40 -0600

    Update

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+
+
+const d = new Date();
+const year = d.getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+
+document.getElementById("lastModified").textContent = document.lastModified;
+
+
+
