840c8d1af98337915b3dfd3746bdf3c75e8f0bb1
ajcurrier <acurrier@byupathway.edu>
2026-09-12 10:04:33 -0600

    Temples Started

---
+++ scripts/temples.js
@@ -0,0 +1,8 @@
+// Get Dates
+
+const d = new Date();
+const year = d.getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+
+document.getElementById("lastModified").textContent = document.lastModified;
