306a5e621512daa98f32e3a2cdfbaf1cafa24a15
ajcurrier <acurrier@byupathway.edu>
2026-09-12 17:00:04 -0600

    Done1

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,3 +6,13 @@ const year = d.getFullYear();
 document.getElementById("currentyear").textContent = year;
 
 document.getElementById("lastModified").textContent = document.lastModified;
+
+
+// Hamburger Menu
+const hamburger = document.getElementById('hamburger-menu');
+const menu = document.getElementById('menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger-x')
+    menu.classList.toggle('show');
+});
