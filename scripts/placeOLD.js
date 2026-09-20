18046c9cdb899ef15c5ca4430f1c01928af6569c
ajcurrier <acurrier@byupathway.edu>
2026-09-20 11:47:23 -0600

    Add Temperature Slider

---
+++ scripts/placeOLD.js
@@ -0,0 +1,23 @@
+const d = new Date();
+const year = d.getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+
+document.getElementById("lastModified").textContent = document.lastModified;
+
+// Weather Variables
+const temperature = 45;
+const windSpeed = 10;
+
+// Wind Chill Calculation
+function calculateWindChill(temp, wind) {
+    if (temp <= 50 && wind > 3) {
+        const windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
+        return windChill.toFixed(1);
+    }
+    return temperature;
+}
+
+const windChill = calculateWindChill(temperature, windSpeed)
+
+document.getElementById("windChill").textContent = windChill
