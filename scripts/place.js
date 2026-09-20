e4e74603709560e3006e15a30d1ca7116d466d5f
ajcurrier <acurrier@byupathway.edu>
2026-09-19 18:54:35 -0600

    change temp

--- scripts/place.js
+++ scripts/place.js
@@ -6,14 +6,14 @@ document.getElementById("currentyear").textContent = year;
 document.getElementById("lastModified").textContent = document.lastModified;
 
 // Weather Variables
-const temperature = 72;
+const temperature = 25;
 const windSpeed = 10;
 
 // Wind Chill Calculation
 function calculateWindChill(temp, wind) {
     if (temp <= 50 && wind > 3) {
         const windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
-        return windChill;
+        return windChill.toFixed(1);
     }
     return temperature;
 }
