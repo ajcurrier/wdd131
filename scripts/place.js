ca27f2b83487bf9486ea012cfbee9fb6752f42ed
ajcurrier <acurrier@byupathway.edu>
2026-09-23 15:12:05 -0600

    remove slider

--- scripts/place.js
+++ scripts/place.js
@@ -2,42 +2,22 @@ const d = new Date();
 const year = d.getFullYear();
 
 document.getElementById("currentyear").textContent = year;
+
 document.getElementById("lastModified").textContent = document.lastModified;
 
-// Weather variables
-let temperature = 45;
+// Weather Variables
+const temperature = 45;
 const windSpeed = 10;
 
-// HTML elements
-const temperatureSlider = document.getElementById("temperatureSlider");
-const temperatureValue = document.getElementById("temperatureValue");
-const windChillValue = document.getElementById("windChill");
-
-// Wind chill calculation
+// Wind Chill Calculation
 function calculateWindChill(temp, wind) {
-  if (temp <= 50 && wind > 3) {
-    const windChill =
-      35.74 +
-      (0.6215 * temp) -
-      (35.75 * Math.pow(wind, 0.16)) +
-      (0.4275 * temp * Math.pow(wind, 0.16));
-
-    return windChill.toFixed(1);
-  }
-
-  return temp.toFixed(1);
+    if (temp <= 50 && wind > 3) {
+        const windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
+        return windChill.toFixed(1);
+    }
+    return temperature;
 }
 
-// Update the displayed temperature and wind chill
-function updateWeather() {
-  temperature = Number(temperatureSlider.value);
+const windChill = calculateWindChill(temperature, windSpeed)
 
-  temperatureValue.textContent = temperature;
-  windChillValue.textContent = calculateWindChill(temperature, windSpeed);
-}
-
-// Update live while dragging the slider
-temperatureSlider.addEventListener("input", updateWeather);
-
-// Display the initial values
-updateWeather();
+document.getElementById("windChill").textContent = windChill
